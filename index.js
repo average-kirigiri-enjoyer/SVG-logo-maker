c105c23527eefad10e8d7602fea61d52ebd021ca
ethan <hertlethan@gmail.com>
2023-09-17 18:57:31 -0400

    added starter files

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ethan (average-kirigiri-enjoyer)
+SCS Boot Camp Module 10 Weekly Challenge - SVG Logo Maker
+Created 2023/09/17
+Last Edited 2023/09/17
+*/
+
+//retrieving packages
+const inquirer = require("inquirer");
+const fs = require("fs");
+

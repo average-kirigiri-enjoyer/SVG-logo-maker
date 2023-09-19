69ef39680eb4b84822d90196116ddb16a5755941
ethan <hertlethan@gmail.com>
2023-09-18 21:26:31 -0400

    added separate folder for shape classes & tests, added basic framework for shape classes, exported shapes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ Last Edited 2023/09/17
 //retrieving packages
 const inquirer = require("inquirer");
 const fs = require("fs");
+const {Shape, Triangle, Square, Circle} = require("./lib/shapes.js");
 
 //array with questions to get data needed from user to generate logo.svg
 const questions = [
@@ -33,8 +34,14 @@ const questions = [
   name: "shapeColour",
 }];
 
+/* following test should pass;
+const shape = new Triangle();
+shape.setColor("blue");
+expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+*/
+
 //function which attempts to write logo data to a .svg file
-function writeToFile(svgData)
+function writeSVGFile(svgData)
 {
   //attempts to write logo data to .svg file logo.svg
   fs.writeFile("./logo.svg", svgData, function(err)
@@ -51,9 +58,37 @@ function writeToFile(svgData)
 }
 
 //function to process logo data from user input & return the result
-processSVGData(data);
+function processSVGData(data)
 {
+  //check that logo text is >= 0 & <= 3
+  //check that user entered a valid colour for text
+  //check that user entered a valid colour for shape
+  //use jest for the above checks, and if any of them fail, eject from function, inform user of error in console, and cancel svg generation
+  //map pixel colours based on shape selected, and overlay text to it
 
+  //create new shape based on the one the user selected
+
+  let shape;
+
+  if (data.shape === "Triangle")
+  {
+
+  }
+  else if (data.shape === "Square")
+  {
+    
+  }
+  if (data.shape === "Circle")
+  {
+    
+  }
+
+  const svgStructure = shape.render();
+  const svgData = `<svg width = "300" height = "200" xmlns = "http://www.w3.org/2000/svg">
+                    ${svgStructure}
+                   </svg>`;
+
+  writeSVGFile(svgData);
 }
 
 //function to prompt the user with questions, the answers to which will be used to generate a logo
@@ -62,10 +97,19 @@ function generateSVG()
   inquirer.prompt(questions)
   .then(function(data)
   {
-    const svgData = processSVGData(data);
-    writeToFile(svgData);
+    processSVGData(data);
   });
 }
 
 //begins SVG generation process
 generateSVG();
+
+/*function testSVG()
+{
+  svgData = `<svg width = "300" height = "200" xmlns = "http://www.w3.org/2000/svg">
+  <polygon points = "150, 18 244, 182 56, 182" fill = "red"/>
+  </svg>`
+  writeSVGFile(svgData);
+}
+
+testSVG();*/
